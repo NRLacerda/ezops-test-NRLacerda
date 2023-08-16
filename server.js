2e884c36914fbf368b7dcdf92a3aab8a3368f195
NRLacerda <nrochalacerda@gmail.com>
2023-08-16 01:44:29 -0300

    atualizando endpoints

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var Message = mongoose.model('Message',{ name : String, message : String})
+var app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+//const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://nrochalacerda:SaEZScQ33W5WNEEg@ezopsnrlacerda.6r4wyyb.mongodb.net/?retryWrites=true&w=majority";
 
-var app = express();
-
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
 
 /*const client = new MongoClient(uri, {
     serverApi: {
@@ -29,16 +35,60 @@ app.use(express.static(__dirname));
     }
   }
 */
+
+/*
+  run().catch(console.dir);
+*/
 mongoose.connect(uri).then(()=>{
-    console.log("Connected.");
+    console.log("Connected to the DB.");
 })
 .catch((err)=>{
     console.log(err);
 })
-  var Message = mongoose.model('Message',{ name : String, message : String})
-/*
-  run().catch(console.dir);*/
-var server = app.listen(3000, ()=>{
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+    socket.on('message', (msg) => {
+        console.log('message: ' + msg);
+      });
+  });
+
+server.listen(3000, ()=>{
     console.log('server is running on port', server.address().port)
 })
 
+
+
+app.get('/messages', async (req, res) => {
+  try {
+    const messages = await Message.find({});
+    res.send(messages);
+  } catch (error) {
+    res.status(500).send('Error fetching messages');
+  }
+});
+
+app.post('/messages', async (req, res) => {
+  try {
+    const message = new Message(req.body);
+    await message.save();
+    res.sendStatus(200);
+  } catch (error) {
+    res.sendStatus(500);
+  }
+});
+  
+
+  
+/*app.post('/messages', async (req, res) => {
+  try {
+    const message = new Message(req.body);
+    await message.save();
+    res.sendStatus(200);
+  } catch (error) {
+    res.sendStatus(500);
+  }
+});*/
